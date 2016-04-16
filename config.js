fdef0ba44f2e60ce27e5404cdd51b4a000b5169e
Aditya Chawla <chawlaaditya8@gmail.com>
2016-04-16 23:57:40 +0530

    twitter

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key: 'HKxLLFQwyUvckbz7ueu06V1PQ',
+  consumer_secret: 'QJLxVqwuFE9nM9LAdK3hkOl3zUmgU5j3iZZDFIMVfMXQbqo580',
+  access_token: '718826975142354944-9BSOEZ5uUkUtlJrV9tneApTknRsQZDY',
+  access_token_secret: 'fuSDbCUhgi4eZAkV5UboSWMHbP5rif9iNswDx0P6LYuaZ'
+
+}
