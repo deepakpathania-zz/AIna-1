fefa2f09b1587f814f27356b79a903f2e035ab4f
Aditya Chawla <chawlaaditya8@gmail.com>
2016-04-17 12:32:10 +0530

    card altered

--- index.js
+++ index.js
@@ -95,26 +95,13 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
+                    "title": "Popularity Graph",
+                    "subtitle": "Graphical Analysis of your product.",
                     "image_url": "https://s3.amazonaws.com/media-p.slid.es/uploads/471316/images/2419155/graph.jpg",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
+                        // "url": "https://www.messenger.com",
+                        "title": "Popularity Percentage: 50.00000000129%"
                     }],
                 }]
             }
